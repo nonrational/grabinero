da69ed98903804451be25eb970bc319e9e5b78a2
Alan Norton <me@alannorton.com>
2013-04-06 14:22:30 -0400

    removed mogoid configurations

--- app.rb
+++ app.rb
@@ -21,15 +21,15 @@ class Lesson < ActiveRecord::Base
     attr_accessible :id, :u_id, :name, :progress
 end
 
-Mongoid.configure do |config|
-  if ENV['MONGOHQ_URL']
-    conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
-    uri = URI.parse(ENV['MONGOHQ_URL'])
-    config.master = conn.db(uri.path.gsub(/^\//, ''))
-  else
-    config.master = Mongo::Connection.from_uri("mongodb://localhost:27017").db('test')
-  end
-end
+# Mongoid.configure do |config|
+#   if ENV['MONGOHQ_URL']
+#     conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+#     uri = URI.parse(ENV['MONGOHQ_URL'])
+#     config.master = conn.db(uri.path.gsub(/^\//, ''))
+#   else
+#     config.master = Mongo::Connection.from_uri("mongodb://localhost:27017").db('test')
+#   end
+# end
 
 helpers do
     def exists(username)
