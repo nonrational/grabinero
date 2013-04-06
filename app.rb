5c4d9e1774a8b52e4735e2281c0cd7eba434b5b9
Alan Norton <me@alannorton.com>
2013-04-06 14:19:21 -0400

    converted tabs to spaces again.

--- app.rb
+++ app.rb
@@ -14,11 +14,11 @@ configure do
 end
 
 class User < ActiveRecord::Base
-	attr_accessible :id, :username, :pass_salt, :pass_hash
+    attr_accessible :id, :username, :pass_salt, :pass_hash
 end
 
 class Lesson < ActiveRecord::Base
-	attr_accessible :id, :u_id, :name, :progress
+    attr_accessible :id, :u_id, :name, :progress
 end
 
 Mongoid.configure do |config|
@@ -32,46 +32,46 @@ Mongoid.configure do |config|
 end
 
 helpers do
-	def exists(username)
-		if User.where(:username => username).first
-			return true
-		else
-			return false
-		end
-	end
+    def exists(username)
+        if User.where(:username => username).first
+            return true
+        else
+            return false
+        end
+    end
 
-	def lesson_done(u_id)
-		if Lesson.where(:u_id => u_id).first
-			return true
-		else
-			return false
-		end
-	end
+    def lesson_done(u_id)
+        if Lesson.where(:u_id => u_id).first
+            return true
+        else
+            return false
+        end
+    end
 
-	def login?
-		if session[:username].nil?
-			return false
-		else
-			return true
-		end
-	end
+    def login?
+        if session[:username].nil?
+            return false
+        else
+            return true
+        end
+    end
 
-	def username
-		return session[:username]
-	end
+    def username
+        return session[:username]
+    end
 end
 
 get '/' do
-	erb :index
+    erb :index
 end
 
 get '/interview/:code' do
-	@code = params[:code]
+    @code = params[:code]
   erb :interview
 end
 
 get '/interview' do
-	redirect '/interview/' + Base64.encode64(rand(1000000).to_s).chomp("=\n")
+    redirect '/interview/' + Base64.encode64(rand(1000000).to_s).chomp("=\n")
 end
 
 get '/lessons' do
@@ -79,14 +79,14 @@ get '/lessons' do
 end
 
 get '/lessons/:lesson' do
-	lesson = params[:lesson]
-	if lesson == "lists"
-		erb :lists
-	elsif lesson == "stacks"
-		erb :stacks
-	elsif lesson == "trees"
-		erb :trees
-	end
+    lesson = params[:lesson]
+    if lesson == "lists"
+        erb :lists
+    elsif lesson == "stacks"
+        erb :stacks
+    elsif lesson == "trees"
+        erb :trees
+    end
 end
 
 get '/contact' do
@@ -98,52 +98,52 @@ not_found do
 end
 
 post '/signup' do
-	if not exists(params[:username])
-		new_id = User.all.last.id + 1
-		salt = BCrypt::Engine.generate_salt
-		hash = BCrypt::Engine.hash_secret(params[:password], salt)
-		User.create(id: new_id, username: params[:username], pass_salt: salt, pass_hash: hash)
-		session[:username] = params[:username]
-		redirect '/'
-	else
-		"this username exists already. go away"
-	end
+    if not exists(params[:username])
+        new_id = User.all.last.id + 1
+        salt = BCrypt::Engine.generate_salt
+        hash = BCrypt::Engine.hash_secret(params[:password], salt)
+        User.create(id: new_id, username: params[:username], pass_salt: salt, pass_hash: hash)
+        session[:username] = params[:username]
+        redirect '/'
+    else
+        "this username exists already. go away"
+    end
 end
 
 post '/login' do
-	if exists(params[:username])
-		user = User.where(:username => params[:username]).first
-		if user.pass_hash == BCrypt::Engine.hash_secret(params[:password], user.pass_salt)
-			session[:username] = params[:username]
-			redirect '/dashboard'
-		end
-	end
-	erb :error
+    if exists(params[:username])
+        user = User.where(:username => params[:username]).first
+        if user.pass_hash == BCrypt::Engine.hash_secret(params[:password], user.pass_salt)
+            session[:username] = params[:username]
+            redirect '/dashboard'
+        end
+    end
+    erb :error
 end
 
 get '/dashboard' do
-	if !session[:username] then
-		session[:previous_url] = request.path
-		@error = 'Sorry but you must log in first'
-		halt erb(:index)
-	end
-	@username = session[:username]
-	erb :dashboard
+    if !session[:username] then
+        session[:previous_url] = request.path
+        @error = 'Sorry but you must log in first'
+        halt erb(:index)
+    end
+    @username = session[:username]
+    erb :dashboard
 end
 
 get '/practice' do
-	if !session[:username] then
-		session[:previous_url] = request.path
-		@error = 'Sorry but you must log in first'
-		halt erb(:index)
-	end
-	@username = session[:username]
-	erb :practice
+    if !session[:username] then
+        session[:previous_url] = request.path
+        @error = 'Sorry but you must log in first'
+        halt erb(:index)
+    end
+    @username = session[:username]
+    erb :practice
 end
 
 get '/logout' do
-	session[:username] = nil
-	redirect '/'
+    session[:username] = nil
+    redirect '/'
 end
 
 #
