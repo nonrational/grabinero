b3527d09e26d799e71dd26a03076c71fa492c901
Evan Drewry <evandrewry@gmail.com>
2013-04-06 19:39:02 -0400

    removed some of the boilerplate stuff from dons other project

--- app.rb
+++ app.rb
@@ -8,9 +8,13 @@ require 'mongoid'
 require 'mongo'
 require 'uri'
 
+state = [:pending, :promised, :fulfilled, :completed]
+code_of_state = Hash[state.map.with_index.to_a]
+
 class Ask
   include Mongoid::Document
   field :description, type: String
+  field :name, type: String
   field :email, type: String
   field :fulfiller, type: String
   field :state, type: Integer
@@ -18,26 +22,26 @@ class Ask
 end
 
 configure :development do
-	enable :sessions
-	set :public_folder, Proc.new { File.join(root, "public") }
-	Mongoid.load!("mongoid.yml")
-    Mongoid.configure do |config|
+  enable :sessions
+  set :public_folder, Proc.new { File.join(root, "public") }
+  Mongoid.load!("mongoid.yml")
+  Mongoid.configure do |config|
     config.sessions = { 
       :default => {
         :hosts => ["localhost:27017"],
         :database => "grabinero"
       }
     }
- 	end
+  end
 end
 
 configure :production do
-	enable :sessions
-	set :public_folder, Proc.new { File.join(root, "public") }
-	uri  = URI.parse(ENV['MONGOLAB_URI'])
-	conn = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
-	db   = conn.db(uri.path.gsub(/^\//, ''))
-	db.collection_names.each { |name| puts name }
+  enable :sessions
+  set :public_folder, Proc.new { File.join(root, "public") }
+  uri  = URI.parse(ENV['MONGOLAB_URI'])
+  conn = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
+  db   = conn.db(uri.path.gsub(/^\//, ''))
+  db.collection_names.each { |name| puts name }
 end
 
 before do
@@ -45,34 +49,39 @@ before do
   p params
 end
 
-
 get '/ask' do
   erb :_submit_ask
 end
 
-post '/ask/create' do
+post '/ask' do
   ask = Ask.create(
     :description => params[:description],
+    :name => params[:name],
     :email => params[:email],
-    :pending => true,
+    :state => code_of_state[:pending],
   )
-  redirect '/ask/show/pending'
+  redirect '/asks/pending'
 end
 
-
-get '/ask/show/all' do
+get '/asks' do
   erb :asks, :locals => { :asks => Ask.order_by([[:createdDateTime, :desc]]) }
 end
-get '/ask/show/pending' do
-  erb :asks, :locals => { :asks => Ask.where(:pending => true).order_by([[:createdDateTime, :desc]]) }
+
+get '/asks/pending' do
+  erb :asks, :locals => { :asks => Ask.where(:state => code_of_state[:pending]).order_by([[:createdDateTime, :desc]]) }
 end
-get '/ask/show/:id' do |id|
-  erb :ask, :locals => { :ask => Ask.find(id) }
+
+get '/ask/:id' do |id|
+  erb :_ask, :locals => { :ask => Ask.find(id) }
 end
+
+=begin
 get '/ask/:id' do |id|
   ask = Ask.find(id)
   ask.to_json
 end
+=end
+
 delete 'ask/:id' do |id|
   Ask.find(id).destroy
 end
@@ -80,23 +89,24 @@ end
 post 'ask/:id/fulfill' do |id|
   #content_type :json  
   ask = Ask.find(id)
-  if params[:email] then ask.add_to_set(params[:email]) end
+  if params[:email]
+    ask.fulfiller = params[:email]
+    ask.state = code_of_state[:promised]
+  end
   #ask.to_json
-  erb :asks, :locals => { :asks => Ask.where(:pending => true) }
+  redirect '/asks/pending'
 end
 
 
 helpers do
+  def fulfill_url(ask)
+    "/ask/#{id}/fulfill"
+  end
+  def show_url(ask)
+    "/ask/show/#{id}"
+  end
   def exists(username)
     if User.where(:username => username).first
-      return true
-    else
-      return false
-    end
-  end
-
-  def lesson_done(u_id)
-    if Lesson.where(:u_id => u_id).first
       return true
     else
       return false
@@ -106,46 +116,18 @@ helpers do
   def login?
     if session[:username].nil?
       return false
-        else
-            return true
-        end
+    else
+      return true
     end
+  end
 
-    def username
-        return session[:username]
-    end
+  def username
+    return session[:username]
+  end
 end
 
 get '/' do
   erb :index
-end
-
-get '/interview/:code' do
-    @code = params[:code]
-  erb :interview
-end
-
-get '/interview' do
-    redirect '/interview/' + Base64.encode64(rand(1000000).to_s).chomp("=\n")
-end
-
-get '/lessons' do
-  erb :lessons
-end
-
-get '/lessons/:lesson' do
-    lesson = params[:lesson]
-    if lesson == "lists"
-        erb :lists
-    elsif lesson == "stacks"
-        erb :stacks
-    elsif lesson == "trees"
-        erb :trees
-    end
-end
-
-get '/contact' do
-  erb :contact
 end
 
 not_found do
@@ -153,79 +135,59 @@ not_found do
 end
 
 post '/signup' do
-    if not exists(params[:username])
-        new_id = User.all.last.id + 1
-        salt = BCrypt::Engine.generate_salt
-        hash = BCrypt::Engine.hash_secret(params[:password], salt)
-        User.create(id: new_id, username: params[:username], pass_salt: salt, pass_hash: hash)
-        session[:username] = params[:username]
-        redirect '/'
-    else
-        "this username exists already. go away"
-    end
+  if not exists(params[:username])
+    new_id = User.all.last.id + 1
+    salt = BCrypt::Engine.generate_salt
+    hash = BCrypt::Engine.hash_secret(params[:password], salt)
+    User.create(id: new_id, username: params[:username], pass_salt: salt, pass_hash: hash)
+    session[:username] = params[:username]
+    redirect '/'
+  else
+    "this username exists already. go away"
+  end
 end
 
 post '/login' do
-    if exists(params[:username])
-        user = User.where(:username => params[:username]).first
-        if user.pass_hash == BCrypt::Engine.hash_secret(params[:password], user.pass_salt)
-            session[:username] = params[:username]
-            redirect '/dashboard'
-        end
+  if exists(params[:username])
+    user = User.where(:username => params[:username]).first
+    if user.pass_hash == BCrypt::Engine.hash_secret(params[:password], user.pass_salt)
+      session[:username] = params[:username]
+      redirect '/dashboard'
     end
-    erb :error
-end
-
-get '/dashboard' do
-    if !session[:username] then
-        session[:previous_url] = request.path
-        @error = 'Sorry but you must log in first'
-        halt erb(:index)
-    end
-    @username = session[:username]
-    erb :dashboard
-end
-
-get '/practice' do
-    if !session[:username] then
-        session[:previous_url] = request.path
-        @error = 'Sorry but you must log in first'
-        halt erb(:index)
-    end
-    @username = session[:username]
-    erb :practice
+  end
+  erb :error
 end
 
 get '/logout' do
-    session[:username] = nil
-    redirect '/'
+  session[:username] = nil
+  redirect '/'
 end
 
 #
 # OAuth Callback
 #
 post '/dwolla/oauth' do
-    return reqlog('/dwolla/oauth', params)
+  return reqlog('/dwolla/oauth', params)
 end
 
 #
 # Payment Information Callback
 #
 post '/dwolla/payment' do
-    return reqlog('/dwolla/payment', params)
+  return reqlog('/dwolla/payment', params)
 end
 
 #
 # Payment Success Callback
 #
 post '/dwolla/success' do
-    return reqlog('/dwolla/payment', params)
+  return reqlog('/dwolla/payment', params)
 end
 
 #
 # Request and Log
 #
 def reqlog(path, params)
-    logger.info("#{path} #{params}")
-    return "#{path} #{params}"
+  logger.info("#{path} #{params}")
+  return "#{path} #{params}"
 end
