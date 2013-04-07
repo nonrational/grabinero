04cd928b2bd8b5fa3edb0f66244415fa5347549b
Sarah Whinnem <sarah@betterment.com>
2013-04-07 02:36:56 -0400

    more jquery fiddling

--- public/js/grabinero.js
+++ public/js/grabinero.js
@@ -13,34 +13,17 @@ $(document).ready(function(){
 
     //show/hide action buttons
     $('.requesteditems .action').hide();
-    $('.requesteditems tr').hover( function() {
-        $('.requesteditems .action').show();
+    $('.requesteditems tr').hover( function(e) {
+        $(e.currentTarget).find('.action').show();
     }, function() {
         $('.requesteditems .action').hide();
     });
 
- //    //show/hide login fields
-	// $('.show_login').click( function() {
- //        $('.login').slideToggle();
- //        //$('.show_login').fadeOut('1000');
- //    });
- //    $('.close_login').click( function() {
- //        $('.login').slideToggle();
- //        //$('.show_login').show();
-        
- //    });
-	
-	// //show/hide site map in footer
-	// $('a.sitemap').click(function(o){
-	// 	$('section.sitemap').slideToggle(400);
-	// });
-	
- //    $(".social-component").click(
- //        function(o){ var t = $(o.target).data("service"); $(".social-box").hide(); $(".social-box." + t).fadeIn(275); return false; }
- //    );
- //    $("span.youtube-uploads").youTubeChannel({userName:"betterment",channel:"uploads",numberToDisplay:1,linksInNewWindow:true});
- //    $('.backtotop a').click(
- //        function () { $('body,html').animate({ scrollTop: 0 }, 800); return false; }
- //    );
+    //show/hide promised form
+    $('.requesteditems .promised .dropdown').hide();
+    $('.requesteditems .promised .action').click( function(e) {
+        $(e.currentTarget).parent().find('.dropdown').slideDown();
+
+    });
 
 });
